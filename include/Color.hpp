ca8a608d3035656b874f10e242a0b9dcbb473f50
saitmilk <33536254+saitmilk@users.noreply.github.com>
2024-09-04 00:02:39 +0800

    feat: antialiasing

--- include/Color.hpp
+++ include/Color.hpp
@@ -1,6 +1,7 @@
 #ifndef COLOR_H
 #define COLOR_H
 
+#include "Interval.hpp"
 #include "Vec3.hpp"
 #include <opencv2/core.hpp>
 #include <opencv4/opencv2/opencv.hpp>
@@ -15,9 +16,10 @@ inline void write_color(std::ostream& out, const Color& pixel_color) {
     auto b = pixel_color.z();
 
     // Translate the [0,1] component values to the byte range [0,255].
-    int rbyte = int(255.999 * r);
-    int gbyte = int(255.999 * g);
-    int bbyte = int(255.999 * b);
+    static const Interval intensity(0.000, 0.999);
+    int rbyte = int(256 * intensity.clamp(r));
+    int gbyte = int(256 * intensity.clamp(g));
+    int bbyte = int(256 * intensity.clamp(b));
 
     // Write out the pixel color components.
     out << rbyte << ' ' << gbyte << ' ' << bbyte << '\n';
@@ -27,9 +29,10 @@ inline void write_color_to_cv(Mat* image, int x, int y, const Color& pixel_color
     auto r = pixel_color.x();
     auto g = pixel_color.y();
     auto b = pixel_color.z();
-    int ir = int(255.999 * r);
-    int ig = int(255.999 * g);
-    int ib = int(255.999 * b);
+    static const Interval intensity(0.000, 0.999);
+    int ir = int(256 * intensity.clamp(r));
+    int ig = int(256 * intensity.clamp(g));
+    int ib = int(256 * intensity.clamp(b));
 
     image->at<Vec3b>(y, x) = Vec3b(ib, ig, ir);
 }
--- src/Main.cpp
+++ src/Main.cpp
@@ -1,95 +1,21 @@
-#include "Color.hpp"
-#include "Hittable.hpp"
+#include "Camera.hpp"
 #include "HittableList.hpp"
-#include "Interval.hpp"
-#include "Rtweekend.hpp"
 #include "Sphere.hpp"
 #include <memory>
 
 using namespace cv;
 using std::make_shared;
 
-bool isOpencvRender = true;
-
-Color ray_color(const Ray& r, const Hittable& world) {
-    HitRecord rec;
-    if (world.hit(r, Interval(0, infinity), rec)) {
-        return 0.5 * (rec.normal + Color(1, 1, 1));
-    }
-
-    Vec3 unit_direction = unit_vector(r.direction());
-    auto a = 0.5 * (unit_direction.y() + 1.0);
-    return (1.0 - a) * Color(1.0, 1.0, 1.0) + a * Color(0.5, 0.7, 1.0);
-}
-
 int main() {
-    // Image
-    auto aspect_ratio = 16.0 / 9.0;
-    int image_width = 400;
+    HittableList world;
 
-    // Calculate the image height, and ensure that it's at least 1.
-    int ideal_image_height = static_cast<int>(image_width / aspect_ratio);
-    int image_height = (ideal_image_height < 1) ? 1 : ideal_image_height;
-
-    // World
-
-    auto world = HittableList(make_shared<Sphere>(Point3(0, 0, -1), 0.5));
     world.add(make_shared<Sphere>(Point3(0, 0, -1), 0.5));
     world.add(make_shared<Sphere>(Point3(0, -100.5, -1), 100));
 
-    // Camera
+    Camera cam;
 
-    auto focal_length = 1.0;
-    auto viewport_height = 2.0;
-    auto viewport_width = viewport_height * (double(image_width) / image_height);
-    auto camera_center = Point3(0, 0, 0);
+    cam.aspect_ratio = 16.0 / 9.0;
+    cam.image_width = 400;
 
-    // Calculate the vectors across the horizontal and down the vertical viewport edges.
-    auto viewport_u = Vec3(viewport_width, 0, 0);
-    auto viewport_v = Vec3(0, -viewport_height, 0);
-
-    // Calculate the horizontal and vertical delta vectors from pixel to pixel.
-    auto pixel_delta_u = viewport_u / image_width;
-    auto pixel_delta_v = viewport_v / image_height;
-
-    // Calculate the location of the upper left pixel.
-    auto viewport_upper_left = camera_center
-        - Vec3(0, 0, focal_length) - viewport_u / 2 - viewport_v / 2;
-    auto pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
-
-    if (isOpencvRender) {
-        Mat* image = new Mat(image_height, image_width, CV_8UC3);
-
-        for (int j = 0; j < image_height; j++) {
-            for (int i = 0; i < image_width; i++) {
-                auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
-                auto pixel_direction = pixel_center - camera_center;
-                Ray r(camera_center, pixel_direction);
-
-                Color pixel_color = ray_color(r, world);
-                write_color_to_cv(image, i, j, pixel_color);
-            }
-            namedWindow("Display Image", WINDOW_AUTOSIZE);
-            imshow("Display Image", *image);
-        }
-
-        waitKey(0);
-    } else {
-        std::cout << "P3\n"
-                  << image_width << ' ' << image_height << "\n255\n";
-
-        for (int j = 0; j < image_height; j++) {
-            std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
-            for (int i = 0; i < image_width; i++) {
-                auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
-                auto pixel_direction = pixel_center - camera_center;
-                Ray r(camera_center, pixel_direction);
-
-                Color pixel_color = ray_color(r, world);
-                write_color(std::cout, pixel_color);
-            }
-        }
-
-        std::clog << "\rDone.                 \n";
-    }
+    cam.renderOpencv(world);
 }
