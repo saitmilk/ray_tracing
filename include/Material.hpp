7a50846c93ec17e38bd5befc8c1ea07b5713a886
saitmilk <33536254+saitmilk@users.noreply.github.com>
2024-09-10 22:32:38 +0800

    feat: dielectrics

--- include/Material.hpp
+++ include/Material.hpp
@@ -56,4 +56,43 @@ private:
     double fuzz;
 };
 
+class Dielectric : public Material {
+public:
+    Dielectric(double refraction_index)
+        : refraction_index(refraction_index) { }
+
+    bool scatter(const Ray& r_in, const HitRecord& rec, Color& attenuation, Ray& scattered) const override {
+        attenuation = Color(1.0, 1.0, 1.0);
+        double ri = rec.front_face ? (1.0 / refraction_index) : refraction_index;
+
+        Vec3 unit_direction = unit_vector(r_in.direction());
+        double cos_theta = std::fmin(dot(-unit_direction, rec.normal), 1.0);
+        double sin_theta = std::sqrt(1.0 - cos_theta * cos_theta);
+
+        bool cannot_refract = ri * sin_theta > 1.0;
+        Vec3 direction;
+
+        if (cannot_refract || reflectance(cos_theta, ri) > random_double()) {
+            direction = reflect(unit_direction, rec.normal);
+        } else {
+            direction = refract(unit_direction, rec.normal, ri);
+        }
+
+        scattered = Ray(rec.p, direction);
+        return true;
+    }
+
+private:
+    // Refractive index in vacuum or air, or the ratio of the material's refractive index over
+    // the refractive index of the enclosing media
+    double refraction_index;
+
+    static double reflectance(double cosine, double refraction_index) {
+        // Use Schlick's approximation for reflectance.
+        auto r0 = (1 - refraction_index) / (1 + refraction_index);
+        r0 = r0 * r0;
+        return r0 + (1 - r0) * std::pow((1 - cosine), 5);
+    }
+};
+
 #endif
--- include/Vec3.hpp
+++ include/Vec3.hpp
@@ -136,4 +136,11 @@ inline Vec3 reflect(const Vec3& v, const Vec3& n) {
     return v - 2 * dot(v, n) * n;
 }
 
+inline Vec3 refract(const Vec3& uv, const Vec3& n, double etai_over_etat) {
+    auto cos_theta = std::fmin(dot(-uv, n), 1.0);
+    Vec3 r_out_perp = etai_over_etat * (uv + cos_theta * n);
+    Vec3 r_out_parallel = -std::sqrt(std::fabs(1.0 - r_out_perp.length_squared())) * n;
+    return r_out_perp + r_out_parallel;
+}
+
 #endif
